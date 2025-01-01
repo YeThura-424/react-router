87a8aec350b082e577e31e2d86efe46d27d7f122
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-01 21:36:37 +0630

    update header component with nav links

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Layout from "./Layout.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Layout />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(

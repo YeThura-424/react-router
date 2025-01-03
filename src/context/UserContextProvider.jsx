45538ec8b946a2448b0067bd798f7c831827b045
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-03 23:00:04 +0630

    fix typo

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = () => {
-  cosnt[(user, setUser)] = useState();
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState();
   return (
-    <UserContext.Provider value={[user, setUser]}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );

9e132bf307db9d014ba0eca38c87b244ad1099f0
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-04 20:16:22 +0630

    use theme context api to toggle theme

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ThemeProvider } from "./context/theme";
 
 const Layout = () => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
